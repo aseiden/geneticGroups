6d7072dd03575c1423c0352632330f33f652ed58
Alex Seiden <alex.seiden@gmail.com>
2017-05-01 15:11:40 -0700

    initialize repo and npm reqs

---
+++ makeGroups.js
@@ -0,0 +1,27 @@
+// Module structure:
+
+// Import necessary packages:
+  // Tabletop: https://www.npmjs.com/package/tabletop
+  // what else?
+
+// Manage data inputs:
+  // # of generations
+  // Cull rate
+  // Gene pool size
+  // Mutation rate
+  // Bonus for affinitiy
+  // Penalty for refusal (technical)
+  // Penalty for refusal (interpersonal)
+  // Penalty for wrong group size (for v2)
+
+// Pull data from google spreadsheet via tabletop
+
+// Generate the initial gene pool randomly
+
+// Begin assessment/cull/breed loop:
+  // Assess the fitness of all combinations in the gene pool
+  // Cull the bottom %
+  // Breed the top % to fill the new gap
+  // Repeat until generation count is met
+
+// Print the most fit combination and associated fitness score to console
