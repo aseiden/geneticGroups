de14b11bf2aa805d4e026fe84d9e0d1aee011b32
Alex Seiden <alex.seiden@gmail.com>
2017-05-01 15:49:46 -0700

    clean and format input data

--- makeGroups.js
+++ makeGroups.js
@@ -14,7 +14,7 @@
   // Penalty for refusal (interpersonal)
   // Penalty for wrong group size (for v2)
 
-// Pull data from google spreadsheet via tabletop
+// Clean data
 
 // Generate the initial gene pool randomly
 
@@ -26,7 +26,7 @@
 
 // Print the most fit combination and associated fitness score to console
 
-var data = require('./preferenceData');
+const data = require('./preferenceData');
 
 const generationCount = 200;
 const cullRate = .1;
@@ -37,4 +37,26 @@ const techRefusal = -3;
 const personalRefusal = -5;
 const wrongSizePentaltyBase = -5;
 
+const phraseToRateTable = {
+  'I would especially enjoy working with this person.': affinityBonus,
+  'I would refuse to work with this person, due to THEIR TECHNICAL ABILITIES': techRefusal,
+  'I would refuse to work with this person, due to OUR INTERPERSONAL ISSUES': personalRefusal
+};
 
+const cleanData = (data) => {
+  var cleanData = {};
+
+  cleanData = data.map((person) => {
+    var cleanEntry = {name: person['yourName'], relationships: {}};
+    for (var key in person) {
+      if (key !== 'timestamp' && key !== 'yourName' && key !== cleanEntry.name) {
+        cleanEntry.relationships[key] = phraseToRateTable[person[key]];
+      }
+    }
+    return cleanEntry;
+  });
+
+  return cleanData;
+};
+
+console.log(cleanData(data));
